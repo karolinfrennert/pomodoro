3b61aaeaeec0148a24870b677f889fa5348adf4e
karolinfrennert <karolinfrennert@gmail.com>
2021-04-16 12:11:46 +0200

    Stop start working

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
-import moment from "moment"
-import momentDurationFormatSetup from 'moment-duration-format'
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format";
 
-momentDurationFormatSetup(moment)
-
-
+momentDurationFormatSetup(moment);
 
 export const TimeLeft = ({ sessionLength }) => {
   const [timeLeft, setTimeLeft] = useState(sessionLength);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const formattedTimeLeft = moment
+    .duration(timeLeft, "s")
+    .format("mm:ss", { trim: false });
 
   useEffect(() => {
-    setTimeLeft(sessionLength)
-  }, [sessionLength])
+    setTimeLeft(sessionLength);
+  }, [sessionLength]);
 
-
-  const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss')
-
+  const isStarted = intervalId !== null;
+  const handleStartStopClick = () => {
+    if (isStarted) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    } else {
+      const newIntervalId = setInterval(() => {
+        setTimeLeft((prev) => {
+          const newTimeLeft = prev - 1;
+          return newTimeLeft >= 0 ? prev - 1 : prev;
+        });
+      }, 1000);
+      setIntervalId(newIntervalId);
+    }
+  };
 
   return (
     <div>
       {formattedTimeLeft}
+      <button onClick={handleStartStopClick}>
+        {isStarted ? "Stop" : "Start"}
+      </button>
     </div>
   );
 };
