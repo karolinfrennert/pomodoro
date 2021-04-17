c9cb3d2af56c5465606e93d3e461adc57e3cc4fc
karolinfrennert <karolinfrennert@gmail.com>
2021-04-17 18:16:49 +0200

    Refactor lifting state

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,43 +1,23 @@
-import { useEffect, useState } from "react";
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 
 momentDurationFormatSetup(moment);
 
-export const TimeLeft = ({ sessionLength }) => {
-  const [timeLeft, setTimeLeft] = useState(sessionLength);
-  const [intervalId, setIntervalId] = useState(null);
-
+export const TimeLeft = ({
+  startStopButtonLable,
+  handleStartStopClick,
+  timerLabel,
+  timeLeft,
+}) => {
   const formattedTimeLeft = moment
     .duration(timeLeft, "s")
     .format("mm:ss", { trim: false });
 
-  useEffect(() => {
-    setTimeLeft(sessionLength);
-  }, [sessionLength]);
-
-  const isStarted = intervalId !== null;
-  const handleStartStopClick = () => {
-    if (isStarted) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    } else {
-      const newIntervalId = setInterval(() => {
-        setTimeLeft((prev) => {
-          const newTimeLeft = prev - 1;
-          return newTimeLeft >= 0 ? prev - 1 : prev;
-        });
-      }, 1000);
-      setIntervalId(newIntervalId);
-    }
-  };
-
   return (
     <div>
-      {formattedTimeLeft}
-      <button onClick={handleStartStopClick}>
-        {isStarted ? "Stop" : "Start"}
-      </button>
+      <p id="timer-label">{timerLabel}</p>
+      <p id="time-left">{formattedTimeLeft}</p>
+      <button onClick={handleStartStopClick}>{startStopButtonLable}</button>
     </div>
   );
 };
