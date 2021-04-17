377f6b3f9fe395d81c944be4911ff92f27badcb3
karolinfrennert <karolinfrennert@gmail.com>
2021-04-17 20:05:23 +0200

    audio works

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { useState, useEffect } from "react";
 import { Break } from "./components/Break";
@@ -6,11 +6,14 @@ import { Break } from "./components/Break";
 import "./App.css";
 import { Session } from "./components/Session";
 import { TimeLeft } from "./components/TimeLeft";
-import { clearInterval } from "timers";
+
+const BREAK_LENGTH = 30;
+const SESSION_LENGTH = 10;
 
 function App() {
-  const [sessionLenght, setSessionLenght] = useState(1500);
-  const [breakLenght, setBreakLenght] = useState(300);
+  const audioElement = useRef<HTMLAudioElement | null>(null);
+  const [sessionLenght, setSessionLenght] = useState(SESSION_LENGTH);
+  const [breakLenght, setBreakLenght] = useState(BREAK_LENGTH);
   const [currentSessionType, setCurrentSessionType] = useState("Session");
   const [timeLeft, setTimeLeft] = useState(sessionLenght);
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
@@ -43,18 +46,24 @@ function App() {
   };
 
   const isStarted = intervalId !== null;
+
   const handleStartStopClick = () => {
     if (isStarted && intervalId) {
       clearInterval(intervalId);
-
       setIntervalId(null);
     } else {
       const newIntervalId = setInterval(() => {
         setTimeLeft((prev) => {
           const newTimeLeft = prev - 1;
-
-          if (newTimeLeft >= 0) {
+          if (newTimeLeft > 0) {
             return prev - 1;
+          }
+          console.log("Changed", prev);
+          if (!audioElement.current) {
+            console.log("AUDIO BROOOKNEEEEENNNNENENN ", audioElement.current);
+          } else {
+            console.log(audioElement.current);
+            audioElement.current.play();
           }
 
           if (currentSessionType === "Session") {
@@ -76,12 +85,22 @@ function App() {
   };
 
   const handleResetButtonClick = () => {
+    if (intervalId === null) {
+      console.log("hey");
+      return;
+    }
+
+    if (audioElement.current === null) {
+      throw Error("Audio Element is not available");
+    } else {
+      audioElement.current.play();
+    }
     clearInterval(intervalId);
     setIntervalId(null);
     setCurrentSessionType("Session");
-    setSessionLenght(60 * 25);
-    setBreakLenght(60 * 5);
-    setTimeLeft(60 * 25);
+    setSessionLenght(SESSION_LENGTH);
+    setBreakLenght(BREAK_LENGTH);
+    setTimeLeft(SESSION_LENGTH);
   };
 
   return (
@@ -105,6 +124,13 @@ function App() {
       <button id="reset" onClick={handleResetButtonClick}>
         Reset
       </button>
+      <audio id="beep" ref={audioElement}>
+        <source
+          src="https://onlineclock.net/audio/options/bird-song.mp3"
+          type="audio/mpeg"
+        ></source>
+      </audio>
+      <button onClick={() => audioElement.current?.play()}> PLAY AUDIO</button>
     </div>
   );
 }
